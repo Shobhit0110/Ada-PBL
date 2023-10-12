20e73227b06e29e2a33f867e9024b21c3659b445
Sumit Dolikar <sumitdeolikar140@gmail.com>
2023-10-12 19:22:51 +0530

    First Commit

---
+++ ada.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+bool isSafe(int i, int j, int row, int col, int arr[][4],
+ vector<vector<bool> > &visited) {
+  
+}
+
+void solveMaze(int arr[][4], int row,int col, int i, int j,
+vector<vector<bool> > &visited, vector<string>& path, string output  ) {
+
+  
+
+
+}
+
+int main() {
+  int maze[4][4] = { {1,0,0,0}, 
+                 {1,1,0, 1},
+                 {1,1,0,0},
+                 {0,1,1,1}};
+
+  if(maze[0][0] == 0) {
+    cout << "No Path Exists " << endl;
+    return 0;
+  }
+
+  int row = 4;
+  int col = 4;
+
+  vector<vector<bool> > visited(row, vector<bool>(col,false));
+  //src ki value k true mark kredete h 
+  visited[0][0] = true;
+
+  vector<string> path;
+  string output = "";
+
+  solveMaze(maze,row,col, 0,0, visited, path, output);
+
+  cout << "printing the results " << endl;
+  for(auto i: path) {
+    cout << i << " ";
+  }
+  cout << endl;
+  if(path.size() == 0 ){
+    cout << "No Path Exists " << endl;
+  }
+
+  
+  return 0;
+}
