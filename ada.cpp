618dc869d0cfaae7541b827e52299ae4d129b5ad
SumitDeolikar <147622644+SumitDeolikar@users.noreply.github.com>
2023-10-16 00:53:24 +0530

    ada.cpp
    isSafe

--- ada.cpp
+++ ada.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 bool isSafe(int i, int j, int row, int col, int arr[][4],
  vector<vector<bool> > &visited) {
-  
+ if( ((i>=0 && i < row) && (j >= 0 && j< col)) &&
+      (arr[i][j] == 1 ) &&
+      (visited[i][j]==false)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 void solveMaze(int arr[][4], int row,int col, int i, int j,
